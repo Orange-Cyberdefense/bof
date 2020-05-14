0e3e475f33df8de80155f31c46bbbbaa4c37f66f
Lex <lex@lex.os>
2020-05-14 18:27:44 +0200

    Dumb fuzzer without boiboite connection is ok

--- examples/dumb_fuzzer.py
+++ examples/dumb_fuzzer.py
@@ -25,22 +25,24 @@ def mutate(frame:bytes) -> (bytes, list):
     frame = bytearray(frame) # Use a mutable object instead
     for _ in range(randint(0,len(frame)-1)): # Random number of times
         idx = randint(0,len(frame)-2) # Random position
-        frame[idx:idx+1] = [randint(0,255)] # Random balue
-        changelog.append(str(idx)) # We save the position with chnged bytes
+        frame[idx:idx+1] = [randint(0,255)] # Random value
+        changelog.append(str(idx)) # We save the position with changed bytes
     return bytes(frame), changelog
 
 def generate_inputs(frame:bytes, trials:int=10) -> bytes:
     """Generate a number of ``inputs` inputs for each type of frame."""
-    for _ in range(trials):
-        mutated_frame, changelog = mutate(bytes(frame))
-        yield mutated_frame, changelog
 
 def fuzz(generator:object, trials:int=10):
     """Retrieve the complete list of inputs"""
     for frame in generator():
-        for mutated_frame, changelog in generate_inputs(bytes(frame), trials):
-            print("+ Sending: {0}".format(mutated_frame))
-            print("++ Type: {0} - Modified fields: {1}".format(frame.sid,
-                                                               ", ".join(changelog)))
+        for _ in range(trials):
+            mutated_frame, changelog = mutate(bytes(frame))
+            yield mutated_frame, frame, changelog
 
-fuzz(all_frames)
+
+# For each existing type of frame (in specification file), create 10 different
+# frames with random mutations and send them to the tested device.
+
+for trial, original, changelog in fuzz(all_frames, 10):
+    print("+ Sending: {0}".format(trial))
+    print("++ Type: {0} - Modified fields: {1}".format(original.sid, ",".join(changelog)))
