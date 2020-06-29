a389c56e457ce24051e5aa6ccc2171b1cd6b7ed2
Lex <lex@lex.os>
2020-06-29 17:32:27 +0200

    cemi_fuzzer update

--- examples/cemi_fuzzer.py
+++ examples/cemi_fuzzer.py
@@ -1,10 +1,12 @@
 from sys import path
 path.append('../')
 
+from datetime import datetime
 from random import getrandbits, randint, choice
 from bof import BOFNetworkError, knx, byte
 
 """Mini fuzzer for cEMI blocks in CONFIGURATION REQUEST frames."""
+
 
 def connect(ip:str, port:int) -> (knx.KnxNet, int):
     """Set item required for connection (source ip and port), saves connection
@@ -45,11 +47,13 @@ def save(event, request, data, response=None):
     """Save request and data mutated that triggered the behavior.
     Should be saved instead of printed :)
     """
-    print("\n")
-    print("--- {0} with data {1}".format(event, data))
-    print("--- Request: {0}".format(bytes(request)))
-    if response:
-        print(response)
+    with open("fuzzing_results.txt", "a") as fd:
+        fd.write("\n")
+        fd.write("--- {0} with data {1}".format(event, data))
+        fd.write("--- Request: {0}".format(bytes(request)))
+        if response:
+            fd.write(response)
+        print("X")
 
 def all_properties(propread_req:knx.KnxFrame) -> (knx.KnxFrame, tuple):
     """Yields one frame for each property existing in all property type
@@ -66,7 +70,8 @@ def random_properties(propread_req:knx.KnxFrame) -> (knx.KnxFrame, str):
     """Yields a frame with properties replaced with random values a 
     random number of times."""
     fields_with_exclusion = [x for x in propread_req.body.cemi.fields if x.name != "message code"]
-    for _ in range(randint(1, 10000)):
+    # for _ in range(randint(1, 10000)):
+    while 1:
         field = choice(fields_with_exclusion)
         save = field.value
         field.value = bytes(map(getrandbits,(8,)*field.size))
@@ -76,33 +81,48 @@ def random_properties(propread_req:knx.KnxFrame) -> (knx.KnxFrame, str):
 def fuzz(generator, initial_frame):
     """Fuzz using a generator to mutate initial frame."""
     try:
-        knxnet, channel = connect("192.168.1.10", 3671)
-        initial_frame.body.communication_channel_id.value = channel
-        sequence_counter = 0
-        for propread_req, data in generator(initial_frame):
-            propread_req.body.sequence_counter.value = sequence_counter
-            try:
-                knxnet.send(propread_req)
-                print(".", end='', flush=True)
-                ack = knxnet.receive()
-                if ack.body.status.value == b"\x00": # Code 00 is OK
-                    propread_con = knxnet.receive()
-                    if propread_con.sid == "CONFIGURATION REQUEST":
-                        ack = knx.KnxFrame(type="CONFIGURATION ACK")
-                        ack.body.communication_channel_id.value = channel
-                        ack.body.sequence_counter.value = sequence_counter
-                        knxnet.send(ack)
-                else:
-                    save("Error in acknowledgement", propread_req, data, ack)
-            except BOFNetworkError:
-                save("Timeout", propread_req, data)
-            sequence_counter += 1
+        triggers = 0
+        total = 0
+        with open("fuzzing_results.txt", "a") as fd:
+            fd.write(datetime.now().strftime("%y-%m-%d-%H:%M:%S")+"\n")
+        while 1: # Each trigger resets the loop.
+            knxnet, channel = connect("192.168.1.10", 3671)
+            initial_frame.body.communication_channel_id.value = channel
+            sequence_counter = 0
+            for propread_req, data in generator(initial_frame):
+                propread_req.body.sequence_counter.value = sequence_counter
+                try:
+                    knxnet.send(propread_req)
+                    print(".", end='', flush=True)
+                    received_ack = knxnet.receive()
+                    if received_ack.body.status.value == b"\x00": # Code 00 is OK
+                        propread_con = knxnet.receive()
+                        if propread_con.sid == "CONFIGURATION REQUEST":
+                            ack_to_send = knx.KnxFrame(type="CONFIGURATION ACK")
+                            ack_to_send.body.communication_channel_id.value = channel
+                            ack_to_send.body.sequence_counter.value = sequence_counter
+                            knxnet.send(ack_to_send)
+                    else:
+                        save("Error in acknowledgement", propread_req, data, received_ack)
+                        triggers += 1
+                        disconnect(knxnet, channel)
+                        break
+                except BOFNetworkError:
+                    save("Timeout", propread_req, data)
+                    triggers += 1
+                    disconnect(knxnet, channel)
+                    break
+                sequence_counter += 1
+                total += 1
     except KeyboardInterrupt:
         print("Cancelled.")
     finally:
         disconnect(knxnet, channel)
+        with open("fuzzing_results.txt", "a") as fd:
+            fd.write("*** ENDED WITH {0} TRIGGERS (Total: {1}) ***\n".format(triggers, sequence_counter))
+            fd.write(datetime.now().strftime("%y-%m-%d-%H:%M:%S")+"\n")
 
 propread = knx.KnxFrame(type="CONFIGURATION REQUEST", cemi="PropRead.req")
 propread.body.cemi.number_of_elements.value = 1
-fuzz(all_properties, propread)
+# fuzz(all_properties, propread)
 fuzz(random_properties, propread)
