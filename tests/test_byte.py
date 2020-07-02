2b77054b1fc88e2e2a017b055eb3f003eb091458
Jonathan Dubois <jonathan.dubois3@gmail.com>
2020-07-02 13:43:47 +0200

    Adding some unit tests on bytes conversion

--- tests/test_byte.py
+++ tests/test_byte.py
@@ -4,7 +4,8 @@
 - Byte conversion to and from ints
 - Byte array resizing
 """
-
+from sys import path
+path.append('../')
 import unittest
 import bof
 
@@ -102,6 +103,67 @@ class Test04ByteAndBits(unittest.TestCase):
         value=15
         field[:size] = bof.byte.int_to_bit_list(value)[-size:]
         self.assertEqual(bof.byte.from_bit_list(field), b"\xF0\x00")
+
+
+
+
+class Test05BitListIntConversion(unittest.TestCase):
+    """Test class for bits transformation to int"""
+    @classmethod
+    def setUpClass(self):
+        bof.set_byteorder('big')
+    def test_01_int_to_bits_and_back(self):
+        """Test conversion from int to bit list, and from bit list to int"""
+        value = 19
+        size = 8
+        result = bof.bit_list_to_int(bof.int_to_bit_list(value, size))
+        self.assertEqual(19, result)
+    def test_02_int_to_bits_and_back_slices(self):
+        """Test conversion from int to bits list, and back, with size reduction"""
+        value = 255
+        result = bof.bit_list_to_int(bof.int_to_bit_list(value, 7))
+        self.assertEqual(127, result)
+    def test_03_int_to_bits_and_back_little_endian(self):
+        """Test conversion from int to bit list, and from bit list to int in little endian"""
+        value = 19
+        size = 8
+        result = bof.bit_list_to_int(bof.int_to_bit_list(value, size, byteorder='little'), byteorder='little')
+        self.assertEqual(19, result)
+    def test_04_int_to_bits_and_back_wrong_endianess(self):
+        
+        value = 19
+        size = 8
+        result = bof.bit_list_to_int(bof.int_to_bit_list(value, size, byteorder='big'), byteorder='little')
+        self.assertNotEqual(19, result)
+    def test_05_int_to_bits_invalid_endianess(self):
+        value = 19
+        with self.assertRaises(bof.BOFProgrammingError):
+            bof.int_to_bit_list(2, byteorder='frite')
+    def test_06_bits_to_int_invalid_endianess(self):
+        value = 19
+        with self.assertRaises(bof.BOFProgrammingError):
+            bof.bit_list_to_int(bof.int_to_bit_list(2), byteorder='frite')
+    def test_07_invalid_input_to_list(self):
+        value = 19
+        with self.assertRaises(bof.BOFProgrammingError):
+            bof.int_to_bit_list("frites")
+    def test_08_invalid_input_to_int(self):
+        value = 19
+        with self.assertRaises(bof.BOFProgrammingError):
+            bof.bit_list_to_int("frites")
+
+class Test06BytesIpv4Conversion(unittest.TestCase):
+    """Test class for bytes conversion to IPV4"""
+    @classmethod
+    def setUpClass(self):
+        bof.set_byteorder('big')
+    def test_01_ipv4_to_bytes_and_back(self):
+        ip = "127.0.0.1"
+        self.assertEqual(bof.to_ipv4(bof.from_ipv4(ip)), "127.0.0.1")
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
