77869387edf2f142587bafe3656d35ff6779bf60
JulienBedel <bedel.ju@gmail.com>
2020-07-31 10:26:03 +0200

    chore(opcua): Add module for OPC UA implementation
    Add init file for OPC UA module, currently containing a simple docstring only.

---
+++ bof/opcua/__init__.py
@@ -0,0 +1,13 @@
+"""
+OPC UA protocol implementation for industrial automation.
+
+The implementation schemes and frame descriptions are based on **IEC 62541**
+standard, **v1.04**. The specification is described in :file:`opcua.json`.
+
+A user should be able to create or alter any frame to both valid and invalid
+format. Therefore, a user can copy, modify or replace the specification file to
+include or change any content they want.
+
+The ``opcua`` submodule has the following content:
+"""
+
