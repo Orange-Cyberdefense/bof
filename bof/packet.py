c169cfe7c203480a320b0fef1cb4846bf820aff3
JulienBedel <account@julienbedel.com>
2021-04-02 10:48:03 +0200

    fix(bof): Typo in show and show2

--- bof/packet.py
+++ bof/packet.py
@@ -55,10 +55,10 @@ class BOFPacket(object):
         yield from self.scapy_pkt
 
     def show(self, dump=False, indent=3, lvl="", label_lvl=""):
-        return self.scapy_pkt.show(dump=False, indent=3, lvl="", label_lvl="")
+        return self.scapy_pkt.show(dump=dump, indent=indent, lvl=lvl, label_lvl=label_lvl)
 
     def show2(self, dump=False, indent=3, lvl="", label_lvl=""):
-        return self.scapy_pkt.show2(dump=False, indent=3, lvl="", label_lvl="")
+        return self.scapy_pkt.show2(dump=dump, indent=indent, lvl=lvl, label_lvl=label_lvl)
 
     def add_payload(self, other, automatic_binding=False):
         """Adds the `other` Scapy payload to the BOFPacket's scapy packet attribute.
