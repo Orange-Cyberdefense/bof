5efdef7e32157a0da798a37e3fb2ac45473b4803
JulienBedel <account@julienbedel.com>
2021-04-02 09:22:07 +0200

    tests(bof): Add test for add_field method
    Tests for :
    - Add basic field
    - Add basic field with value
    - Add PacketField

--- tests/test_packet.py
+++ tests/test_packet.py
@@ -6,33 +6,47 @@
 """
 
 import unittest
+
+from scapy.contrib.modbus import ModbusADURequest
+from scapy.fields import ByteField, PacketField
+from scapy.layers.inet import TCP
+
 from bof.packet import *
+
 
 class Test01PacketConstruct(unittest.TestCase):
     """Test class for raw BOFPacket initialization.
     BOFPacket are not supposed to be instantiated directly.
     """
+
     def test_0101_bofpacket_construct(self):
         """Test empty constructor."""
         pkt = BOFPacket()
         self.assertEqual(pkt.name, "BOFPacket")
+
     def test_0102_bofpacket_child_construct(self):
         """Test that we can build an object inheriting from BOFPacket."""
+
         class OtterPacket(BOFPacket):
             pass
+
         pkt = OtterPacket()
         self.assertEqual(pkt.name, "OtterPacket")
 
+
 class Test02ScapyLayers(unittest.TestCase):
     """Test class to make sure that we access Scapy layer content."""
+
     def test_0201_bofpacket_scapylayer_integrated(self):
         """Test that we can build BOFPacket from layer in scapy library."""
         from scapy.contrib import modbus
         class Modbus(BOFPacket):
             def __init__(self):
                 self.scapy_pkt = modbus.ModbusADURequest()
+
         modbus_pkt = Modbus()
         self.assertEqual(modbus_pkt.name, "ModbusADU")
+
     def test_0202_bofpacket_scapylayer_standalone(self):
         """Test that we can build BOFPacket from layer as standalone file.
         The standalone file shall have a protocol written with scapy format.
@@ -41,8 +55,10 @@ class Test02ScapyLayers(unittest.TestCase):
         class KNX(BOFPacket):
             def __init__(self):
                 self.scapy_pkt = knx.KNXnetIP()
+
         knx_pkt = KNX()
         self.assertEqual(knx_pkt.scapy_pkt.name, "KNXnet/IP")
+
     def test_0203_bofpacket_scapylayer_addpayload_base_scapy(self):
         """Test that we can add a Scapy layer as a payload for a BOFPacket.
         """
@@ -53,6 +69,7 @@ class Test02ScapyLayers(unittest.TestCase):
         pkt.scapy_pkt = TCP()
         pkt.add_payload(ModbusADURequest())
         # TODO: see how to check for correct packet construction (ie : show2..)
+
     def test_0204_bofpacket_scapylayer_addpayload_base_bof(self):
         """Test that we can add a BOFPacket as payload for another BOFPacket.
         """
@@ -63,6 +80,7 @@ class Test02ScapyLayers(unittest.TestCase):
         pkt.scapy_pkt = TCP()
         pkt.add_payload(ModbusADURequest())
         # TODO: see how to check for correct packet construction (ie : show2..)
+
     def test_0205_bofpacket_scapylayer_addpayload_automatic_scapy(self):
         """Test that we can add an unbound Scapy packet as payload and automatically perform the binding.
         """
@@ -73,30 +91,57 @@ class Test02ScapyLayers(unittest.TestCase):
         pkt.scapy_pkt = ModbusADURequest()
         pkt.add_payload(TCP(), automatic_binding=True)
         # TODO: see how to check for correct packet construction (ie : show2..)
-        def test_0206_bofpacket_scapylayer_addlayer_no_automatic_scapy(self):
-        """Test that we can add an unbound Scapy packet as payload on purpose.
-        """
+
+    def test_0206_bofpacket_scapylayer_addlayer_no_automatic_scapy(self):
+        """Test that we can add an unbound Scapy packet as payload on purpose."""
         pkt = BOFPacket()
         pkt.scapy_pkt = ModbusADURequest()
         pkt.add_payload(TCP(), automatic_binding=False)
         # TODO: see how to check for correct packet construction (ie : show2..)
 
 
-class Test02PacketBuiltins(unittest.TestCase):
+class Test03PacketBuiltins(unittest.TestCase):
     """Test class for raw BOFPacket builtin function overload."""
+
     @classmethod
     def setUpClass(self):
         self.pkt = BOFPacket()
 
     def test_0301_bofpacket_bytes(self):
         """Test that bytes() prints Scapy packet bytes."""
-        self.assertEqual(bytes(self.pkt), b"") # TODO
+        self.assertEqual(bytes(self.pkt), b"")  # TODO
+
     def test_0302_bofpacket_len(self):
         """Test that len() prints Scapy packet length."""
-        self.assertEqual(len(self.pkt), 0) # TODO
+        self.assertEqual(len(self.pkt), 0)  # TODO
+
     def test_0302_bofpacket_str(self):
         """Test that str() prints Scapy packet string."""
         self.assertEqual(str(self.pkt), "BOFPacket: BOFPacket")
+
     def test_0302_bofpacket_iter(self):
         """Test that str() prints Scapy packet fields."""
-        self.assertEqual([x.name for x in self.pkt], []) # TODO
+        self.assertEqual([x.name for x in self.pkt], [])  # TODO
+
+
+class Test04PacketManipulations(unittest.TestCase):
+    """Test class to make sure that we can dynamically manipulate packet content (ex : add fields)."""
+
+    @classmethod
+    def setUpClass(self):
+        self.bof_pkt = BOFPacket()
+
+    def test_0401_bofpacket_scapylayer_addfield(self):
+        """Test that we can add a new field at the end of an existing BOFPacket's scapy_pkt."""
+        self.bof_pkt.add_field(ByteField("new_field", 0x01))
+        self.assertEqual(self.bof_pkt.scapy_pkt.new_field, 0x01)
+
+    def test_0402_bofpacket_scapylayer_addfield_value(self):
+        """Test that we can add a new field at the end of an existing BOFPacket's scapy_pkt with a specified value."""
+        self.bof_pkt.add_field(ByteField("new_field", 0x01), 0x02)
+        self.assertEqual(self.bof_pkt.scapy_pkt.new_field, 0x02)
+
+    def test_0403_bofpacket_scapylayer_addfield_packet(self):
+        """Test that we can add a Packet Field at the end of an existing BOFPacket's scapy_pkt."""
+        self.bof_pkt.add_field(PacketField("new_packet_field", TCP(sport=12345), TCP))
+        self.assertEqual(self.bof_pkt.scapy_pkt.new_packet_field.sport, 12345)
